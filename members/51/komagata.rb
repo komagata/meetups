b6f962552de2fb656b067a0ec78bc892ef661749
Masaki Komagata <komagata@gmail.com>
2017-09-12 17:31:53 +0900

    Create komagata.rb

---
+++ members/51/komagata.rb
@@ -0,0 +1,13 @@
+# @komagata
+
+* [github](https://github.com/komagata)
+* [twitter](https://twitter.com/komagata)
+* [facebook](https://www.facebook.com/komagata1111)
+
+Fjord, llcという会社で働いています。おもにrailsを使っています。
+
+## 最近気になる事
+
+* GKE
+* 広島の観光名所
+* RubyMineの便利な使い方
